c3f17d6bf629e2a35b425559f1abec14b34b235e
Rik Clegg <rik.clegg@gmail.com>
2017-09-09 13:25:13 +0100

    Add project files.

---
+++ EasyMKTSample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyMKTSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
